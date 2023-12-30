5b54d6d110bf785e39d847fb55fbff9355c79c50
Jithin Renji <jithin0904@gmail.com>
2023-12-29 17:48:31 -0700

    Update identifiable structs to make more sense

--- MShare/Model/Contact.swift
+++ MShare/Model/Contact.swift
@@ -7,23 +7,14 @@
 
 import Foundation
 
-struct Contact: Identifiable, Hashable {
-    var id: UUID
+struct Contact: Identifiable {
+    let id = UUID()
     var name: String
     var pubkey: String
     
     init(name: String, pubkey: String) {
-        self.id = UUID()
         self.name = name
         self.pubkey = pubkey
-    }
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-    
-    static func == (lhs: Contact, rhs: Contact) -> Bool {
-        return lhs.id == rhs.id
     }
     
     static func example() -> Contact {
---
+++ MShare/Model/Message.swift
@@ -0,0 +1,8 @@
+//
+//  Message.swift
+//  MShare
+//
+//  Created by Jithin Renji on 12/29/23.
+//
+
+import Foundation
